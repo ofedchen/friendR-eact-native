87b63f989bc25547941ad93449d075a386262ca8
Olha Fedchenko <ofedchen@gmail.com>
2025-08-12 15:38:51 +0200

    homepage and add friend page

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { createStaticNavigation, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Friend database</Text>
+      <Button onPress={() => navigation.navigate('AddFriendPage')} title='Add your friends' />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+    
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation />;
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1a0966ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 40,
+    marginBlock: 20
+  }
 });
